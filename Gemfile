c962db64931d6492193fe7c38d02ee274de7be96
Sebastián Gallardo <gallaseba@gmail.com>
2016-10-18 18:00:26 -0300

    Added gem google-drive-ruby

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'google-api-client', '~> 0.9'
-gem 'google-id-token', '~> 1.3'
+gem 'google_drive'
+#gem 'google-api-client', '~> 0.9'
+#gem 'google-id-token', '~> 1.3'
 gem 'sinatra', '~> 1.4'
 gem 'dotenv'
+gem 'pry'
--- cloud-app.rb
+++ cloud-app.rb
@@ -1,5 +1,41 @@
 require 'sinatra'
+require 'dotenv'
+
+configure do
+	Dotenv.load
+end
 
 get '/' do
   erb :home
+end
+
+get '/sign' do
+	credentials = Google::Auth::UserRefreshCredentials.new(
+	 client_id: ENV['GOOGLE_CLIENT_ID'],
+	 client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+	 scope: [
+	   "https://www.googleapis.com/auth/drive",
+	   "https://spreadsheets.google.com/feeds/",
+	 ],
+	 redirect_uri: "/signed")
+	auth_url = credentials.authorization_uri
+	redirect auth_url
+end
+
+get '/signed' do
+	credentials = Google::Auth::UserRefreshCredentials.new(
+	 client_id: ENV['GOOGLE_CLIENT_ID'],
+	 client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+	 scope: [
+	   "https://www.googleapis.com/auth/drive",
+	   "https://spreadsheets.google.com/feeds/",
+	 ])
+	credentials.code = params[:authorization_code]
+	credentials.fetch_access_token!
+	session = GoogleDrive::Session.from_credentials(credentials)
+	files = session.files
+	binding.pry
+	session.files do |file|
+	  p file
+	end
 end
